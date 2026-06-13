1eb6fffdcf03176675f1597b98102c76ac5e0fe9
Neel-2002 <njcool2002@gmail.com>
2026-06-13 12:11:35 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings:
+    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+    EMBEDDING_MODEL = "models/gemini-embedding-001"
+    LLM_MODEL = "gemini-2.5-flash"
+
+    VECTOR_TOP_K = 4
+
+settings = Settings()
---
+++ normalization/spacy.py
@@ -0,0 +1,27 @@
+# import spacy
+
+# def text_cleaning(text: str):
+#     nlp = spacy.load('en_core_web_sm')
+
+    
+
+
+
+
+
+
+
+
+
+# class CLEAN:
+#     def __init__(self):
+#         self.user_prompt = input("Enter a prompt: ")
+#     def text_cleaning(self):
+#         import spacy
+#         nlp = spacy.load('en_core_web_sm')
+#         docs = nlp(space.user_prompt)
+#         cleaned_tokens = [token.lemma_ for token in docs if not token.is_stop]
+#         return ' '.join(cleaned_tokens)
+# user1 = CLEAN()
+# result = user1.text_cleaning()
+# print(result)
